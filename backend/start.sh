c53775978160e14a9fe7b9c9674dcf4b98af7097
annierome1 <112560966+annierome1@users.noreply.github.com>
2025-02-22 12:50:36 -0500

    update start.sh

--- backend/start.sh
+++ backend/start.sh
@@ -3,4 +3,4 @@
 pip install -r requirements.txt --no-cache-dir
 
 export PYTHONPATH=$PYTHONPATH:/app
-uvicorn backend.api:app --host 0.0.0.0 --port ${PORT:-8006}
+uvicorn api:app --host 0.0.0.0 --port ${PORT:-8006}
